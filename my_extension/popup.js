77c3cd89355eb402cd2cf2c540bc047248848012
Healthy Coder <sinyaniso.writer@gmail.com>
2024-09-15 00:55:09 +0000

    Iteration 1: Getting thing started

---
+++ my_extension/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("my_button");
+  button.addEventListener("click", function() {
+    console.log("Button clicked!");
+    // Add your code here to make something cool happen!
+  });
+});
